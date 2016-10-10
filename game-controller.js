0a82217b36256d342a68b947aace65143770c48b
Chad Rohr <chadrohr@yahoo.com>
2016-10-10 16:44:50 -0600

    eod realy no change

--- game-controller.js
+++ game-controller.js
@@ -1,10 +1,18 @@
 function startGame() {
     new GameManager(function(gm) {
+ 
+// gm.getGumshoe()
+// $('#city').on('click', function(event))
+// console.log(gm.getGumshoe())
+
+
+
+
 
     })
    // JQUERY HOOKS GO DOWN HERE :)
     // You will need to use Event Delegation
-//   gs
+
    
 
    
