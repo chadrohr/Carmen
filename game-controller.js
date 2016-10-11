e1db2b936f6781a3724099250e51c33ede07e941
Chad Rohr <chadrohr@yahoo.com>
2016-10-11 08:42:11 -0600

    nearly working. missing search update

--- game-controller.js
+++ game-controller.js
@@ -1,24 +1,55 @@
 function startGame() {
-    new GameManager(function(gm) {
-var gumshoe = gm.getGumshoe()
-$('#city').text(gumshoe.city)
-$('#budget').text(gumshoe.budget)
 
+    //    // JQUERY HOOKS GO DOWN HERE :)
+    //     // You will need to use Event Delegation
 
-//gm.getGumshoe()
-// $('#city').on('click', function(event))
-console.log(gm.getGumshoe())
+    new GameManager(function (gm) {
+        var cities = gm.getCities();
+        var city = gm.getGumshoe().city;
+        var budget = gm.getGumshoe().budget;
 
+        cities.forEach(function (city) {
+            $('#cities').append(`<button id="${city}">${city}</button>`)
+        });
+        $('#cities').on('click', 'button', function () {
+            gm.flyTo(this.id, update)
+        })
+        $('#search').on('click', function () {
+            gm.checkGuess(victory)
+        })
 
+        $('#budget').html(budget);
+        $('#current-city').html(city);
+        $('#search-cost').html(gm.getSearchCost)
+        $('body').on('click', '.city-button', function () {
+            gm.flyTo(this.id, function (a) {
+                update();
+            })
+        })
 
-
-
+        function victory(victory) {
+            if (victory) {
+                $('#victory').html(`<h1>You found her!</h1>`)
+            } else {
+                $('#victory').html(`<h4>Oops try again!!</h4>`)
+            }
+            update()
+            updateBudget()
+        }
+        function update() {
+            debugger
+            gm.getGumshoe()
+            var city = gm.getGumshoe().city;
+            var budget = gm.getGumshoe().budget;
+            $('#budget').html(budget);
+            $('#current-city').html(city);
+        }
+        function updateBudget() {
+            var gumshoe = gm.getGumshoe();
+            $('#budget').html(gumshoe.budget)
+            $('#city').html(gumshoe.city)
+        }
+        updateBudget()
     })
-   // JQUERY HOOKS GO DOWN HERE :)
-    // You will need to use Event Delegation
-
-   
-
-   
-  }
+}
 $('#start').on('click', startGame);
